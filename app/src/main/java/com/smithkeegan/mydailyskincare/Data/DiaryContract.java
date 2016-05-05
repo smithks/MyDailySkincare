b4e953f96711f3570c70e82041ad0f72ea79d9ca
smithks <keeganssmith12@gmail.com>
2016-05-05 15:17:29 -0400

    Create database contract and helper classes.

--- app/src/main/java/com/smithkeegan/mydailyskincare/Data/DiaryContract.java
+++ app/src/main/java/com/smithkeegan/mydailyskincare/Data/DiaryContract.java
@@ -3,6 +3,7 @@ package com.smithkeegan.mydailyskincare.Data;
 import android.provider.BaseColumns;
 
 /**
+ * Contract used to maintain database constants.
  * @author Keegan Smith
  * @since 5/3/2016
  */
@@ -12,6 +13,69 @@ public final class DiaryContract {
     public DiaryContract(){}
 
     public static abstract class DiaryEntry implements BaseColumns{
+        public static final String TABLE_NAME = "DiaryEntry";
+        public static final String COLUMN_DATE = "Date";
+        public static final String COLUMN_PHOTO = "Photo";
+        public static final String COLUMN_GENERAL_CONDITION = "GeneralCondition";
+        public static final String COLUMN_FOREHEAD_CONDITION = "ForeheadCondition";
+        public static final String COLUMN_NOSE_CONDITION = "NoseCondition";
+        public static final String COLUMN_CHEEK_CONDITION = "CheekCondition";
+        public static final String COLUMN_LIPS_CONDITION = "LipsCondition";
+        public static final String COLUMN_CHIN_CONDITION = "ChinCondition";
+        public static final String COLUMN_EXERCISE = "Exercise";
+        public static final String COLUMN_DIET = "Diet";
+        public static final String COLUMN_HYGIENE = "Hygiene";
+        public static final String COLUMN_WATER_INTAKE = "WaterIntake";
+        public static final String COLUMN_ON_PERIOD = "OnPeriod";
+    }
 
+    public static abstract class ConditionModifier implements BaseColumns{
+        public static final String TABLE_NAME = "ConditionModifier";
+        public static final String COLUMN_DATE = "Date";
+        public static final String COLUMN_MODIFIER_ID = "ModifierID";
+        public static final String COLUMN_LOCATION = "Location";
+    }
+
+    public static abstract class Modifier implements BaseColumns{
+        public static final String TABLE_NAME = "Modifier";
+        public static final String COLUMN_NAME = "Name";
+    }
+
+    public static abstract class DiaryEntryRoutine implements BaseColumns{
+        public static final String TABLE_NAME = "DiaryEntryRoutine";
+        public static final String COLUMN_DATE = "Date";
+        public static final String COLUMN_ROUTINE_ID = "RoutineID";
+    }
+
+    public static abstract class Routine implements BaseColumns{
+        public static final String TABLE_NAME = "Routine";
+        public static final String COLUMN_NAME = "Name";
+        public static final String COLUMN_TIME = "Time";
+    }
+
+    public static abstract class RoutineProduct implements BaseColumns{
+        public static final String TABLE_NAME = "RoutineProduct";
+        public static final String COLUMN_ROUTINE_ID = "RoutineID";
+        public static final String COLUMN_PRODUCT_ID = "Product";
+    }
+
+    public static abstract class Product implements BaseColumns{
+        public static final String TABLE_NAME = "Product";
+        public static final String COLUMN_BRAND = "Brand";
+        public static final String COLUMN_NAME = "Name";
+        public static final String COLUMN_TYPE = "Name";
+    }
+
+    public static abstract class ProductIngredient implements BaseColumns{
+        public static final String TABLE_NAME = "ProductIngredient";
+        public static final String COLUMN_PRODUCT_ID = "ProductID";
+        public static final String COLUMN_INGREDIENT_ID = "IngredientID";
+    }
+
+    public static abstract class Ingredient implements BaseColumns{
+        public static final String TABLE_NAME = "Ingredients";
+        public static final String COLUMN_NAME = "Name";
+        public static final String COLUMN_IRRITANT = "Irritant";
+        public static final String COLUMN_COMMENT = "Comment";
     }
 }
---
+++ app/src/main/java/com/smithkeegan/mydailyskincare/Data/DiaryDbHelper.java
@@ -0,0 +1,141 @@
+package com.smithkeegan.mydailyskincare.Data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * @author Keegan Smith
+ * @since 5/4/2016
+ */
+public class DiaryDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Diary.db";
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String INTEGER_TYPE = " INTEGER";
+    private static final String NOT_NULL = " NOT NULL";
+    private static final String AUTOINCREMENT = " AUTOINCREMENT";
+    private static final String COMMA_SEP = ",";
+
+    private static final String SQL_DIARY_ENTRY_TABLE =
+            "CREATE TABLE " + DiaryContract.DiaryEntry.TABLE_NAME +
+                " (" +
+                DiaryContract.DiaryEntry.COLUMN_DATE + INTEGER_TYPE + " PRIMARY KEY" + NOT_NULL + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_PHOTO + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_GENERAL_CONDITION + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_FOREHEAD_CONDITION + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_NOSE_CONDITION + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_CHEEK_CONDITION + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_LIPS_CONDITION + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_CHIN_CONDITION + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_EXERCISE + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_DIET + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_HYGIENE + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_WATER_INTAKE + INTEGER_TYPE + COMMA_SEP +
+                DiaryContract.DiaryEntry.COLUMN_ON_PERIOD + INTEGER_TYPE +
+                " );";
+
+    private static final String SQL_MODIFIER_TABLE =
+            "CREATE TABLE" + DiaryContract.Modifier.TABLE_NAME +
+                "( " +
+                DiaryContract.Modifier._ID + INTEGER_TYPE + " PRIMARY KEY" + NOT_NULL + AUTOINCREMENT + COMMA_SEP +
+                DiaryContract.Modifier.COLUMN_NAME + TEXT_TYPE + NOT_NULL +
+                " );";
+
+    private static final String SQL_CONDITION_MODIFIER_TABLE =
+            "CREATE TABLE " + DiaryContract.ConditionModifier.TABLE_NAME +
+                " (" +
+                DiaryContract.ConditionModifier.COLUMN_DATE + INTEGER_TYPE + NOT_NULL + COMMA_SEP +
+                DiaryContract.ConditionModifier.COLUMN_MODIFIER_ID + INTEGER_TYPE + NOT_NULL + COMMA_SEP +
+                DiaryContract.ConditionModifier.COLUMN_LOCATION + TEXT_TYPE + NOT_NULL + COMMA_SEP +
+                "FOREIGN KEY (" + DiaryContract.ConditionModifier.COLUMN_DATE + ") REFERENCES " + DiaryContract.DiaryEntry.TABLE_NAME + "(" + DiaryContract.DiaryEntry.COLUMN_DATE + ")" + COMMA_SEP +
+                "FOREIGN KEY (" + DiaryContract.ConditionModifier.COLUMN_MODIFIER_ID + ") REFERENCES " + DiaryContract.Modifier.TABLE_NAME + "("+ DiaryContract.Modifier._ID + ")" + COMMA_SEP +
+                "PRIMARY KEY (" + DiaryContract.ConditionModifier.COLUMN_DATE + COMMA_SEP + DiaryContract.ConditionModifier.COLUMN_MODIFIER_ID + COMMA_SEP + DiaryContract.ConditionModifier.COLUMN_LOCATION + ")" +
+                " );";
+
+    private static final String SQL_ROUTINE_TABLE =
+            "CREATE TABLE " + DiaryContract.Routine.TABLE_NAME +
+                " (" +
+                DiaryContract.Routine._ID + INTEGER_TYPE + " PRIMARY KEY" + AUTOINCREMENT + NOT_NULL + COMMA_SEP +
+                DiaryContract.Routine.COLUMN_NAME + TEXT_TYPE + NOT_NULL + COMMA_SEP +
+                DiaryContract.Routine.COLUMN_TIME + INTEGER_TYPE +
+                " );";
+
+    private static final String SQL_DIARY_ENTRY_ROUTINE_TABLE =
+            "CREATE TABLE " + DiaryContract.DiaryEntryRoutine.TABLE_NAME +
+                " (" +
+                DiaryContract.DiaryEntryRoutine.COLUMN_DATE + INTEGER_TYPE + NOT_NULL + COMMA_SEP +
+                DiaryContract.DiaryEntryRoutine.COLUMN_ROUTINE_ID + INTEGER_TYPE +NOT_NULL + COMMA_SEP +
+                "FOREIGN KEY (" + DiaryContract.DiaryEntryRoutine.COLUMN_DATE + ") REFERENCES " + DiaryContract.DiaryEntry.TABLE_NAME + "(" + DiaryContract.DiaryEntry.COLUMN_DATE + ")" + COMMA_SEP +
+                "FOREIGN KEY (" + DiaryContract.DiaryEntryRoutine.COLUMN_ROUTINE_ID + ") REFERENCES" + DiaryContract.Routine.TABLE_NAME + "(" + DiaryContract.Routine._ID + ")" + COMMA_SEP +
+                "PRIMARY KEY (" + DiaryContract.DiaryEntryRoutine.COLUMN_DATE + COMMA_SEP + DiaryContract.DiaryEntryRoutine.COLUMN_ROUTINE_ID + ")" +
+                " );";
+
+    private static final String SQL_PRODUCT_TABLE =
+            "CREATE TABLE " + DiaryContract.Product.TABLE_NAME +
+                " (" +
+                DiaryContract.Product._ID + INTEGER_TYPE + " PRIMARY KEY" + AUTOINCREMENT + NOT_NULL + COMMA_SEP +
+                DiaryContract.Product.COLUMN_BRAND + TEXT_TYPE + COMMA_SEP +
+                DiaryContract.Product.COLUMN_NAME + TEXT_TYPE + NOT_NULL + COMMA_SEP +
+                DiaryContract.Product.COLUMN_TYPE + TEXT_TYPE + NOT_NULL +
+                " );";
+
+    private static final String SQL_ROUTINE_PRODUCT_TABLE =
+            "CREATE TABLE " + DiaryContract.RoutineProduct.TABLE_NAME +
+                " (" +
+                DiaryContract.RoutineProduct.COLUMN_ROUTINE_ID + INTEGER_TYPE + NOT_NULL + COMMA_SEP +
+                DiaryContract.RoutineProduct.COLUMN_PRODUCT_ID + INTEGER_TYPE + NOT_NULL + COMMA_SEP +
+                "FOREIGN KEY (" + DiaryContract.RoutineProduct.COLUMN_ROUTINE_ID + ") REFERENCES " + DiaryContract.Routine.TABLE_NAME + "(" + DiaryContract.Routine._ID + ")" + COMMA_SEP +
+                "FOREIGN KEY (" + DiaryContract.RoutineProduct.COLUMN_PRODUCT_ID + ") REFERENCES " + DiaryContract.Product.TABLE_NAME + "(" + DiaryContract.Product._ID + ")" + COMMA_SEP +
+                "PRIMARY KEY (" + DiaryContract.RoutineProduct.COLUMN_ROUTINE_ID + COMMA_SEP + DiaryContract.RoutineProduct.COLUMN_PRODUCT_ID + ")" +
+                " );";
+
+    private static final String SQL_INGREDIENT_TABLE =
+            "CREATE TABLE " + DiaryContract.Ingredient.TABLE_NAME +
+                    " (" +
+                    DiaryContract.Ingredient._ID + INTEGER_TYPE + " PRIMARY KEY " + AUTOINCREMENT + NOT_NULL + COMMA_SEP +
+                    DiaryContract.Ingredient.COLUMN_NAME + TEXT_TYPE + NOT_NULL + COMMA_SEP +
+                    DiaryContract.Ingredient.COLUMN_IRRITANT + INTEGER_TYPE + NOT_NULL + COMMA_SEP +
+                    DiaryContract.Ingredient.COLUMN_COMMENT + TEXT_TYPE +
+                    " );";
+
+    private static final String SQL_PRODUCT_INGREDIENT_TABLE =
+            "CREATE TABLE " + DiaryContract.ProductIngredient.TABLE_NAME +
+                    " (" +
+                    DiaryContract.ProductIngredient.COLUMN_PRODUCT_ID + INTEGER_TYPE + NOT_NULL + COMMA_SEP +
+                    DiaryContract.ProductIngredient.COLUMN_INGREDIENT_ID + INTEGER_TYPE + NOT_NULL + COMMA_SEP +
+                    "FOREIGN KEY (" + DiaryContract.ProductIngredient.COLUMN_PRODUCT_ID + ") REFERENCES " + DiaryContract.Product.TABLE_NAME + "(" + DiaryContract.Product._ID + ")" + COMMA_SEP +
+                    "FOREIGN KEY (" + DiaryContract.ProductIngredient.COLUMN_INGREDIENT_ID + ") REFERENCES " + DiaryContract.Ingredient.TABLE_NAME + "(" + DiaryContract.Ingredient._ID + ")" + COMMA_SEP +
+                    "PRIMARY KEY (" + DiaryContract.ProductIngredient.COLUMN_PRODUCT_ID + COMMA_SEP + DiaryContract.ProductIngredient.COLUMN_INGREDIENT_ID + ")" +
+                    " );";
+
+
+    private static final String SQL_TABLES =
+            SQL_DIARY_ENTRY_TABLE +
+            SQL_MODIFIER_TABLE +
+            SQL_CONDITION_MODIFIER_TABLE +
+            SQL_ROUTINE_TABLE +
+            SQL_DIARY_ENTRY_ROUTINE_TABLE +
+            SQL_PRODUCT_TABLE +
+            SQL_ROUTINE_PRODUCT_TABLE +
+            SQL_INGREDIENT_TABLE +
+            SQL_PRODUCT_INGREDIENT_TABLE;
+
+
+    public DiaryDbHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_TABLES);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
