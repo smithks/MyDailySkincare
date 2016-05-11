289a006db22bf5a240018ba1d8801f791303076b
smithks <keeganssmith12@gmail.com>
2016-05-11 05:52:28 -0400

    Update ingredients

---
+++ app/src/main/java/com/smithkeegan/mydailyskincare/IngredientDetailFragment.java
@@ -0,0 +1,19 @@
+package com.smithkeegan.mydailyskincare;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Keegan Smith
+ * @since 5/10/2016
+ */
+public class IngredientDetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+        return inflater.inflate(R.layout.fragment_ingredient_detail,container,false);
+    }
+}
